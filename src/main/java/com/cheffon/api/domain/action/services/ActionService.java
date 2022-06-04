ab464551242b340ee81f1a61f8abf18ec2c41774
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-06-04 17:59:09 -0300

    IS11-Correção de formatação.

--- src/main/java/com/cheffon/api/domain/action/services/ActionService.java
+++ src/main/java/com/cheffon/api/domain/action/services/ActionService.java
@@ -6,17 +6,19 @@ import com.cheffon.api.domain.action.repositories.ActionRepository;
 import com.cheffon.api.domain.action.services.interfaces.ActionDomainService;
 import com.cheffon.api.shared.exceptions.domain.EntidadeNaoEncontradaException;
 
-public final class ActionService<V,T> implements ActionDomainService {
+public final class ActionService<V, T> implements ActionDomainService {
 
- private final ActionRepository<V, T> actionRepository;
+    private final ActionRepository<V, T> actionRepository;
 
- public ActionService(ActionRepository<V, T> actionRepository){this.actionRepository = actionRepository;}
+    public ActionService(ActionRepository<V, T> actionRepository) {
+        this.actionRepository = actionRepository;
+    }
 
     @Override
     public Action validar(Long id) throws EntidadeNaoEncontradaException {
         Action action = actionRepository.buscar(id);
-        if(action == null){
-            throw new EntidadeNaoEncontradaException( Action.class );
+        if (action == null) {
+            throw new EntidadeNaoEncontradaException(Action.class);
         }
         return action;
     }
